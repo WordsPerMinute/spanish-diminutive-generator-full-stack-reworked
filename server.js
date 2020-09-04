f198f6e78293fde2c2e42b0960d25bd6928711d3
WordsPerMinute <git@coreyhodge.net>
2020-09-03 21:56:09 -0600

    server.js troubleshooting for heroku deployment

--- server.js
+++ server.js
@@ -74,10 +74,10 @@ app.get('/images', async (req, res) => {
 
 // required for proper deployment to Heroku, along with adding the heroku-postbuild that goes into client, runs npm install and npm build, and adding config keys in Heroku project settings
 if (process.env.NODE_ENV) {
-  app.use(express.static('client/build'));
+  app.use(express.static('front-end-react/build'));
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'front-end-react', 'build', 'index.html'));
   });
 }
 
