80706ae8739ef624551916799331e354d1f35ae1
WordsPerMinute <git@coreyhodge.net>
2020-09-03 21:29:02 -0600

    changes for heroku

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
 const port = process.env.PORT || 3001
 const path = require('path'); 
 const axios = require('axios');
@@ -34,7 +33,7 @@ app.get('/images', async (req, res) => {
   res.json(searchData)
 })
 
-// the scraper version
+// the scraper in progress
 // app.get('/proxy', async (req, response) => {
 
 //   let searchData = await axios.get('https://www.google.com/search?tbm=isch&q=casa', {
@@ -73,20 +72,13 @@ app.get('/images', async (req, res) => {
 
 // })
 
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-// app.use('/proxy', createProxyMiddleware({
-//     target: 'https://serpapi.com/search',
-//     changeOrigin: true
-// }));
-
-
-
-// app.get('*', (req, res) => {
-//    res.sendFile(path.resolve(__dirname, 'index.html'));
-// });
+// required for proper deployment to Heroku, along with adding the heroku-postbuild that goes into client, runs npm install and npm build, and adding config keys in Heroku project settings
+if (process.env.NODE_ENV) {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Listening on PORT ${port}!`))
