5005e45f7b9db8244eac2db33d37b7c64c7315cf
WordsPerMinute <git@coreyhodge.net>
2020-09-03 21:05:48 -0600

    got CORS working on the backend, moved all the fetch logic from WordUtils function to node server

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const port = process.env.PORT || 3001
@@ -6,45 +7,71 @@ const axios = require('axios');
 
 const app = express();
 
-app.get('/proxy', async (request, response) => {
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials", "true")
+  next();
+});
 
-  let searchData = await axios.get('https://www.google.com/search?tbm=isch&q=casa', {
-    headers: {
-      "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36"
-    }
-  })
-  .then(function (response) {
-    
-    // console.log(response.data);
-    // response.header = "Access-Control-Allow-Origin", "*"
-    // console.log(response.data)
-    return response.data
+// the SerpAPI version
+app.get('/images', async (req, res) => {
+
+  console.log('env:', process.env.API_KEY)
+
+  const word = req.query.word
+
+  let searchData = await axios.get(`https://serpapi.com/search?engine=google&q=${word}&tbm=isch&ijn=0&api_key=${process.env.API_KEY}`)
+  .then(function (res) {
+    return res.data
   })
   .catch(function (error) {
     // handle error
     console.log(error);
   })
 
-  console.log('hit')
-  console.log(typeof await searchData)  
-  response.header("Access-Control-Allow-Origin", "*");
-  response.send(await searchData)
+  console.log(searchData)
+  res.json(searchData)
+})
+
+// the scraper version
+// app.get('/proxy', async (req, response) => {
+
+//   let searchData = await axios.get('https://www.google.com/search?tbm=isch&q=casa', {
+//     headers: {
+//       "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36"
+//     }
+//   })
+//   .then(function (response) {
+//     // response.header = "Access-Control-Allow-Origin", "*"
+//     // console.log(response.data)
+//     return response.data
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   })
+
+//   console.log('hit')
+//   console.log(typeof await searchData)  
+//   response.header("Access-Control-Allow-Origin", "*");
+//   response.send(await searchData)
 
   
-  // console.log(request.query)
-  // const GSR = require('google-search-results-nodejs')
-  // const client = new GSR.GoogleSearchResults("537ff1f6bbd0441800874a6c8d5db51d0809a944a6e1442231ea8368147555ed")
-  // client.json({
-  // engine: request.query.engine,
-  // q: request.query.q,
-  // tbm: request.query.tbm
-  // }, (result) => {
-  //   console.log(result)
-  //   response.header("Access-Control-Allow-Origin", "*");
-  //   response.json(result)
-  // })
+//   // console.log(req.query)
+//   // const GSR = require('google-search-results-nodejs')
+//   // const client = new GSR.GoogleSearchResults("537ff1f6bbd0441800874a6c8d5db51d0809a944a6e1442231ea8368147555ed")
+//   // client.json({
+//   // engine: req.query.engine,
+//   // q: req.query.q,
+//   // tbm: req.query.tbm
+//   // }, (result) => {
+//   //   console.log(result)
+//   //   response.header("Access-Control-Allow-Origin", "*");
+//   //   response.json(result)
+//   // })
 
-})
+// })
 
 // app.use(function (req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
